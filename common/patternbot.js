553ba76c7d271ee15efcf05416d19a9fa15ddaf4
krystyna-Macleod <macl0312@algonquinlive.com>
2018-03-19 08:21:15 -0400

    Add code fromt pattern library

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/TheCreativeKrys/Documents/Semester 4/Web Development IV/Week 9/geohub
- * @version 1521461744943
+ * @version 1521462054294
  */
-const patternManifest_1521461744943 = {
+const patternManifest_1521462054294 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -828,5 +828,5 @@ const patternManifest_1521461744943 = {
   }
 };
 
-patternBotIncludes(patternManifest_1521461744943);
+patternBotIncludes(patternManifest_1521462054294);
 }());
